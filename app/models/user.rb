2588fea605d46c627166a8f15bb6f419883698bd
m9795 <106650955+m9795@users.noreply.github.com>
2022-06-26 18:24:38 +0900

    [Fix]誤字修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  has_many :followings, through: :relationships, source: :follo
+  has_many :followings, through: :relationships, source: :followed
   
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
@@ -21,5 +21,17 @@ class User < ApplicationRecord
   
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+  
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
   end
 end
